8b78791d8907bcbd626316af8cc1f6b13f08c8af
transitvanman1 <57574548+transitvanman@users.noreply.github.com>
2022-05-10 01:54:15 +0000

    Add comments and simplify variable names

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -1,9 +1,13 @@
-class Natural_number_class(object):
-    def sum_100_natural_numbers(self):
+## Ok so i was able to knock this up after alot of research.
+## Its not how I thought id do it but Im sure this has fewer
+## lines of code than I would have originally wrote.
+
+class number(object):
+    def sum_100_numbers(self):
         sum = 0
         for i in range(1, 101):
             sum = sum + i
         return sum
 #Create an Object
-object_sum = Natural_number_class()
-print(object_sum.sum_100_natural_numbers())
+object_sum = number()
+print(object_sum.sum_100_numbers())
